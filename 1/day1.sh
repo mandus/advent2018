0598c31f3762975b6a9cbd9646ddf9bc03b2405a
Åsmund Ødegård <mandus@gmail.com>
2018-12-01 18:22:31 +0100

    part2

--- 1/day1.sh
+++ 1/day1.sh
@@ -5,8 +5,27 @@
 IFS=$'\n' read -d '' -r -a values < $1
 
 declare -i freq=0
-for val in "${values[@]}" ; do
-    curfreq=$freq
-    freq=$((freq "$val"))
-    echo "Current frquency ${curfreq}, change if ${val}; resulting frequency ${freq}"
+declare -A seen
+
+output() {
+    curfreq=$1
+    val=$2
+    freq=$3
+    theend=$4
+    echo "Current frquency ${curfreq}, change of ${val}; resulting frequency ${freq}${theend}"
+}
+
+while : ; do
+    for val in "${values[@]}" ; do
+        curfreq=$freq
+        freq=$((freq "$val"))
+        theend="."
+        if [ ${seen[$freq]+_} ] ; then
+            output $curfreq $val $freq ", which has already been seen."
+            exit 0
+        else
+            seen[$freq]='y'
+        fi
+        output $curfreq $val $freq "."
+    done
 done
